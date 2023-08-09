471a5191af747a975c71bbb91affd482ff31b736
jmg <macgreene14@gmail.com>
2023-08-09 12:13:41 -0700

    fixing styling nonrender for card, hidden card

--- src/app/page.js
+++ src/app/page.js
@@ -59,6 +59,24 @@ export default function App() {
                       <h2 className=" text-gray-600 font-md uppercase text-5xl m-6">
                         Resorts
                       </h2>
+                      {/* insert hidden card to force styling on card component */}
+                      <div className="hidden">
+                        <a href={"url"} target="_blank">
+                          <div className="w-full rounded overflow-hidden shadow-lg border border-solid">
+                            <div className="px-6 py-4">
+                              <div className="text-gray-700 ml-2 font-semibold text-xl mb-2">
+                                {"name"}
+                              </div>
+                              <p className="text-gray-700 text-base">
+                                {"description"}
+                              </p>
+                            </div>
+                            <div className="px-6 pt-4 pb-2">
+                              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"></span>
+                            </div>
+                          </div>
+                        </a>
+                      </div>
                       <ResultsContainer resorts={renderedResorts} />
                     </div>
                   </div>
