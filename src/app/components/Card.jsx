4aeacd5b1cfaf0dfb521ae4e379d982f76a9fa56
jmg <macgreene14@gmail.com>
2023-08-11 15:58:00 -0700

    adjust card responsiveness

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,9 +1,9 @@
 "use client";
 export function Card({ resort, flight }) {
   return (
-    <div className="snap-start">
+    <div className="snap-start m-2">
       <a href={resort.properties.website} target="_blank">
-        <div className="min-w-[400px] min-h-[175px] lg:w-full h-full mx-2 shadow-md rounded-lg border-solid hover:shadow:lg hover:border-4 m-4">
+        <div className="min-w-[400px] lg:min-w-full min-h-[200px] lg:w-full h-full shadow-md rounded-lg border-solid border-2 hover:shadow:lg hover:border-4 my-1">
           <div className="p-4">
             <div className="text-gray-700 font-semibold text-md mb-2">
               {resort.properties.name}
--- src/app/components/ResultsContainer.jsx
+++ src/app/components/ResultsContainer.jsx
@@ -4,7 +4,7 @@ import { Card } from "./Card.jsx";
 
 export function ResultsContainer({ resorts, flights }) {
   return (
-    <div className="overflow-auto rounded-lg bg-white shadow h-[32vh] lg:h-[80vh] flex flex-row lg:flex-col p-6 snap-y">
+    <div className="overflow-auto rounded-lg bg-white shadow h-[32vh] lg:h-[80vh] flex flex-row lg:flex-col snap-y">
       {resorts.map((resort, index) => {
         return (
           <Card
