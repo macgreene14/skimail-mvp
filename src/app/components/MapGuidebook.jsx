144013870946928f49ede00aab73b936858226de
jmg <macgreene14@gmail.com>
2023-11-06 11:39:05 -0700

    added basic crosshair feature, test on mobile

--- src/app/components/MapGuidebook.jsx
+++ src/app/components/MapGuidebook.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import "mapbox-gl/dist/mapbox-gl.css";
 import LogCam from "../../../utils/LogCam";
@@ -8,6 +8,8 @@ export default function MapGuideBook({ resort }) {
 
   const mapContainer = useRef(null);
   const map = useRef(null);
+
+  const [selected, setSelected] = useState("init");
 
   // assign camera settings
   const cam_resort_lat = resort[0].properties.cam_resort_lat;
@@ -32,73 +34,92 @@ export default function MapGuideBook({ resort }) {
     map.current.addControl(new mapboxgl.FullscreenControl(), "top-right");
   });
 
+  // useEffect(() => {
+  //   map.current.on(
+  //     "moveend",
+  //     () => {
+  //       // Read out coordinates
+  //       var center = map.current.getCenter();
+  //       var zoom = map.current.getZoom().toFixed(2);
+
+  //       console.log("Latitude:", center.lat);
+  //       console.log("Longitude:", center.lng);
+  //       console.log("Zoom:", zoom);
+  //     },
+  //     [],
+  //   );
+  // });
+
+  // useEffect(() => {
+  //   map.current.on("mouseenter", ["road-path-bg"], (e) => {
+  //     let coordinates = e.lngLat; //click event coordinates
+  //     const name = e.features[0].properties.name;
+  //     const geometry = e.features[0].geometry;
+
+  //     // Check if feature property type is "piste"
+  //     if (e.features[0].properties.type !== "piste") {
+  //       return;
+  //     }
+
+  //     // Exit if name property not available
+  //     if (!e.features[0].properties.hasOwnProperty("name")) {
+  //       return;
+  //     }
+
+  //     // Create and add popup
+  //     const popup = addPopupTrail(coordinates, name);
+
+  //     // Remove popup
+  //     map.current.on("mouseleave", "road-path-bg", () => {
+  //       map.current.getCanvas().style.cursor = "";
+  //       popup.remove();
+  //     });
+
+  //     // Clean up on dismount
+  //     return () => {
+  //       popup.remove();
+  //     };
+  //   });
+
+  //   map.current.on("click", ["road-path-bg"], (e) => {
+  //     let coordinates = e.lngLat; //click event coordinates
+  //     const name = e.features[0].properties.name;
+
+  //     // Check if feature property type is "piste"
+  //     if (e.features[0].properties.type !== "piste") {
+  //       return;
+  //     }
+
+  //     // Exit if name property not available
+  //     if (!e.features[0].properties.hasOwnProperty("name")) {
+  //       return;
+  //     }
+
+  //     // Create and add popup
+  //     const popup = addPopupTrail(coordinates, name);
+
+  //     return () => {
+  //       popup.remove();
+  //     };
+  //   });
+  // });
+
   useEffect(() => {
-    map.current.on(
-      "moveend",
-      () => {
-        // Read out coordinates
-        var center = map.current.getCenter();
-        var zoom = map.current.getZoom().toFixed(2);
+    map.current.on("moveend", () => {
+      // Read out coordinates
+      var center = map.current.getCenter();
 
-        console.log("Latitude:", center.lat);
-        console.log("Longitude:", center.lng);
-        console.log("Zoom:", zoom);
-      },
-      [],
-    );
-  });
+      var features = map.current.queryRenderedFeatures(
+        map.current.project(center),
+        { layers: ["road-path-bg"] },
+      );
 
-  useEffect(() => {
-    map.current.on("mouseenter", ["road-path-bg"], (e) => {
-      let coordinates = e.lngLat; //click event coordinates
-      const name = e.features[0].properties.name;
-      const geometry = e.features[0].geometry;
-
-      // Check if feature property type is "piste"
-      if (e.features[0].properties.type !== "piste") {
-        return;
+      if (features.length) {
+        // If there is a marker, select it.
+        // selectMarker(features[0]);
+        console.log(features[0]);
+        setSelected(features[0].properties.name);
       }
-
-      // Exit if name property not available
-      if (!e.features[0].properties.hasOwnProperty("name")) {
-        return;
-      }
-
-      // Create and add popup
-      const popup = addPopupTrail(coordinates, name);
-
-      // Remove popup
-      map.current.on("mouseleave", "road-path-bg", () => {
-        map.current.getCanvas().style.cursor = "";
-        popup.remove();
-      });
-
-      // Clean up on dismount
-      return () => {
-        popup.remove();
-      };
-    });
-
-    map.current.on("click", ["road-path-bg"], (e) => {
-      let coordinates = e.lngLat; //click event coordinates
-      const name = e.features[0].properties.name;
-
-      // Check if feature property type is "piste"
-      if (e.features[0].properties.type !== "piste") {
-        return;
-      }
-
-      // Exit if name property not available
-      if (!e.features[0].properties.hasOwnProperty("name")) {
-        return;
-      }
-
-      // Create and add popup
-      const popup = addPopupTrail(coordinates, name);
-
-      return () => {
-        popup.remove();
-      };
     });
   });
 
@@ -126,6 +147,28 @@ export default function MapGuideBook({ resort }) {
   return (
     <div ref={mapContainer} className="z-10 h-full w-full">
       {/* <LogCam map={map} /> */}
+      <div
+        style={{
+          top: "50%",
+          left: "50%",
+          position: "absolute",
+          zIndex: 1,
+          fontSize: "20px",
+        }}
+      >
+        +
+      </div>
+      <div
+        style={{
+          top: "10px",
+          left: "10px",
+          position: "absolute",
+          zIndex: 1,
+          fontSize: "20px",
+        }}
+      >
+        {selected}
+      </div>
     </div>
   );
 }
